dd5e330eee905e9775c249452006d64120e8d5e5
Ray Jose Perez <oturaniko@gmail.com>
2026-01-18 19:10:45 +0100

    Primer intento limpio

---
+++ app/page.js
@@ -0,0 +1,9 @@
+'use client';
+export default function Home() {
+  return (
+    <main style={{ backgroundColor: '#0f172a', minHeight: '100vh', color: 'white', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
+      <h1 style={{ color: '#3b82f6' }}>SISTEMA ALMACÉN V2</h1>
+      <p>¡Si ves esto, lo logramos!</p>
+    </main>
+  );
+}
