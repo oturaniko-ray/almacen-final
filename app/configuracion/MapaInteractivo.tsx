808316c1118f59be8dbccdb2cfd3a4fc7b155815
Ray Jose Perez <oturaniko@gmail.com>
2026-01-28 05:23:07 +0100

    config 5

--- app/configuracion/MapaInteractivo.tsx
+++ app/configuracion/MapaInteractivo.tsx
@@ -1,23 +1,22 @@
 'use client';
 import { MapContainer, TileLayer, Marker, useMapEvents } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
-import L from 'leaflet';
-import { useEffect } from 'react';
-
-// Corregir iconos de Leaflet
-const icon = L.icon({
-  iconRetinaUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon-2x.png',
-  iconUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon.png',
-  shadowUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-shadow.png',
-  iconSize: [25, 41],
-  iconAnchor: [12, 41]
-});
+import L from 'leaflet'; // Después de instalar @types/leaflet esto funcionará perfecto
 
 interface Props {
   lat: number;
   lng: number;
   onLocationChange: (lat: number, lng: number) => void;
 }
+
+// Fix para los iconos de Leaflet en Next.js
+const customIcon = new L.Icon({
+  iconUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon.png',
+  iconRetinaUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon-2x.png',
+  shadowUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-shadow.png',
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+});
 
 function MapEvents({ onLocationChange }: { onLocationChange: (lat: number, lng: number) => void }) {
   useMapEvents({
@@ -37,14 +36,13 @@ export default function MapaInteractivo({ lat, lng, onLocationChange }: Props) {
       center={[lat, lng]} 
       zoom={17} 
       style={{ height: '100%', width: '100%' }}
-      scrollWheelZoom={true}
     >
       <TileLayer
         url="https://{s}.google.com/vt/lyrs=s,h&x={x}&y={y}&z={z}"
         subdomains={['mt0', 'mt1', 'mt2', 'mt3']}
         attribution='&copy; Google Maps'
       />
-      <Marker position={[lat, lng]} icon={icon} />
+      <Marker position={[lat, lng]} icon={customIcon} />
       <MapEvents onLocationChange={onLocationChange} />
     </MapContainer>
   );
