4b5f40d4e2fa52b383f6454d1e6db3a731d2ce1d
Ray Jose Perez <oturaniko@gmail.com>
2026-02-10 14:18:40 +0100

    inicio 6

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ import {
   NotificacionSistema, 
   CampoEntrada, 
   ContenedorPrincipal 
-} from ''./configuracion/componentes';
+} from ''.app/configuracion/componentes';
 
 const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!);
 
